96ed4ad1c9c1fe0e4157ef31d1b995843a2244af
Javohir <javohirsadullayev2024@gmail.com>
2025-12-01 20:38:21 +0500

    Program.cs ; added

--- Program.cs
+++ Program.cs
@@ -1,5 +1,2 @@
 ﻿Console.WriteLine("Hello Git!");
-<<<<<<< HEAD
-=======
-Console.WriteLine("Collaborator git kodini yozdi")
->>>>>>> a1503e81c1d075639e6c3d9ca39b2b88fbfbec8a
+Console.WriteLine("Collaborator git kodini yozdi");
